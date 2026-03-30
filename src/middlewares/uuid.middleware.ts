050cb7968dcb09c3e387748c8a641ec682fbb139
NohYusung <nes0903@dobedub.com>
2026-03-30 22:33:06 +0900

    del: pm2 배포시 자꾸 문제생기는 uuid 부분삭제

--- src/middlewares/uuid.middleware.ts
+++ src/middlewares/uuid.middleware.ts
@@ -1,6 +1,5 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import type { NextFunction, Request, Response } from 'express';
-import { v7 as uuid } from 'uuid';
 import { Context, ContextKey } from '@common/context';
 
 @Injectable()
@@ -8,7 +7,7 @@ export class UUIDMiddleware implements NestMiddleware {
     constructor(private readonly context: Context) {}
 
     use(req: Request, _: Response, next: NextFunction) {
-        const traceId = req.get('x-request-id') || uuid();
+        const traceId = req.get('x-request-id');
         this.context.set(ContextKey.TRACE_ID, traceId);
         next();
     }
